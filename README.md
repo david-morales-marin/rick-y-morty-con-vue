ea2b03b90eb1232b25037259d7dcd1c68471b900
david-morales-marin <91661131+david-morales-marin@users.noreply.github.com>
2023-03-11 09:38:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rick-y-morty-con-vue
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-deck7y)
---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <main>
+  <RouterView></RouterView>
+  </main>
+  </div>
+
+  <footer
+    class="
+      fixed
+      bottom-0
+      left-0
+      w-full
+      flex
+      items-center
+      justify-center
+      bg-slate-900
+      py-2
+    "
+  >
+    <RouterLink
+      to="/"
+      class="
+        px-4
+        py-2
+        rounded-lg
+        bg-cyan-400
+        shadow-lg shadow-lime-500/50
+        border-2 border-black
+        outline outline-1 outline-white
+      "
+    >
+      Home
+    </RouterLink>
+  </footer>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
